413f95e9d05dfa642a6d69a6c0aa9914b18d0b93
stantoha <78742661+stantoha@users.noreply.github.com>
2022-08-06 17:43:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 const findTravelBox = document.querySelectorAll('.find__travel__background') ;
-const findTravelBoxBackgrounds=[ "1","2","3","4","5","6","7","8","9","10"/* ,"11","12","13","14","15",
-"16","17","18","19","20","21","22","23","24", "25" */ ];
-
+const findTravelBoxBackgrounds=[ "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"/* ,
+"16","17","18","19","20","21","22","23","24", "25" */ ],
+findTravelBoxBackgroundsVertical=[ /* "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15", */
+"16","17","18","19","20","21","22","23","24", "25" ];
 
 
 function findTravelChangeBG(){
     findTravelBox.forEach((item, i) => {
         let timerId=setTimeout(function log(){
-
-            item.style.cssText = `
-            background:url("img/bg/(${findTravelBoxBackgrounds[i]}).jpg") no-repeat ${findTravelBoxBackgrounds[i]}0% 50%;
+            if(document.documentElement.clientWidth>991){
+                item.style.cssText = `
+            background:url("img/(${findTravelBoxBackgrounds[i]}).png") no-repeat  ;
             background-size:cover;`;
             if(i<findTravelBoxBackgrounds.length-1 || i===0){
                 i++;
-                timerId=setTimeout(log,7000);
+                timerId=setTimeout(log,10000);
             }
             else if(i==findTravelBoxBackgrounds.length-1){
                 i=0;
-                timerId=setTimeout(log,7000);
+                timerId=setTimeout(log,10000);
             }
+            }
+            else{
+                item.style.cssText = `
+            background:url("img/(${findTravelBoxBackgroundsVertical[i]}).png") no-repeat  ;
+            background-size:cover;`;
+            if(i<findTravelBoxBackgroundsVertical.length-1 || i===0){
+                i++;
+                timerId=setTimeout(log,10000);
+            }
+            else if(i==findTravelBoxBackgroundsVertical.length-1){
+                i=0;
+                timerId=setTimeout(log,10000);
+            }
+            }
+
+            
         },100);
 });
 } 
